c54264431d37bf6f8340f9d60834e16ba5c86b7a
Dimitris Efstathiou <defstat@gmail.com>
2017-02-09 13:59:01 +0200

    travis

--- tools/travis/run-tests.sh
+++ tools/travis/run-tests.sh
@@ -49,7 +49,7 @@ mkdir ${FILESDIR}
 
 # Run data build suite
 if [[ "$TEST" == "mysql" ]]; then
-    if [ \( "$TEST_CURRENT_PKP_PLUGIN" -eq 1 \) ]; then
+    if [[ "$TEST_CURRENT_PKP_PLUGIN" == "1" ]]; then
         echo "====================WE ARE USING THE PLUGINS RUN_TEST===================="
         ./plugins/$PKP_PLUGIN_CATEGORY/$PKP_PLUGIN_NAME/tools/travis/runAllTests.sh -m
     else
